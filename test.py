83f78dfb4491427cc9aee8c7997f9b6d89bdf064
wind <1075569209@qq.com>
2022-06-25 10:28:31 +0800

    又刷题啦！

--- test.py
+++ test.py
@@ -1,43 +1,125 @@
+import re
 
-def generateMatrix(n):
-	"""
-	:type n: int
-	:rtype: List[List[int]]
-	"""
-	ans = [[0]*n for _ in range(n)]
-	i,j = 0,0
-	# 左上角为零点，向右为x轴递增，向下为y轴递增，定义方向为：右，下，左，上
-	directions = [[0,1],[1,0],[0,-1],[-1,0]] 
 
-	def go(a,direction,table):
-		# 从a点坐标按照direction走到b点，返回b点坐标, 超出边界或已经存在值则返回 False
-		x = a[0]+direction[0]
-		y = a[1]+direction[1]
-		if x >= len(table) or y >= len(table[0]):
-			return False
-		elif table[x][y] != 0:
-			return False
-		return [x, y]
+class MyLinkedList:
 
-	location = [0,-1]
-	num = 1
-	while True:
-		for i in range(4):
-			flag = True
-			while flag:
-				flag = go(location, directions[i], ans)
-				if flag is not False:
-					location = flag
-					ans[location[0]][location[1]] = num
-					num += 1
-				
-				if num > n*n:
-					break
-			if num > n*n:
-				break
-		if num > n*n:
-			break
-	return ans
+    def __init__(self):
+        self.val = None
+        self.next = None
 
-ans = generateMatrix(3)
-print(ans)
+
+    def get(self, index: int) -> int:
+        cur_node = self
+        val = self.val
+        for i in range(index):
+            if val is None:
+                return -1
+            
+            cur_node = cur_node.next
+            val = cur_node.val
+            
+        return val
+            
+
+    def addAtHead(self, val: int) -> None:
+        if self.val is None:
+            self.val = val
+        else:
+            self.addAtIndex(0,val)
+            print()
+        
+        
+
+
+    def addAtTail(self, val: int) -> None:
+        if self.val is None:
+            self.val = val
+            return
+        
+        head = MyLinkedList()
+        head.next = self
+        cur = head
+        
+        while cur.next is not None:
+            cur = cur.next
+        node = MyLinkedList()
+        node.next = None
+        node.val = val
+        cur.next = node
+
+    def addAtIndex(self, index: int, val: int) -> None:
+        if self.val is None and index == 0:
+            self.val = val
+            return
+        
+        head = MyLinkedList()
+        head.next = self
+        pre = head
+        for i in range(index):
+            pre = pre.next
+            if pre.next is None:
+                break
+        node = MyLinkedList()
+        node.val = val
+        node.next = pre.next
+        pre.next = node
+        
+        if index == 0:
+            
+            p = head.next
+            q = p.next
+            q.val, p.val = p.val, q.val
+            if q is not None:
+                p.next = q.next
+                q.next = p
+
+        
+
+    def deleteAtIndex(self, index: int) -> None:
+        head = MyLinkedList()
+        head.next = self
+        pre = head
+        if index == 0:
+            if self.next is None:
+                self.val = None
+            else:
+                self.val = self.next.val
+                self.next = self.next.next
+        else:
+            for i in range(index):
+                pre = pre.next
+                if pre.next is None:
+                    break
+            if pre.next is not None:
+                pre.next = pre.next.next
+        
+
+
+
+# Your MyLinkedList object will be instantiated and called as such:
+
+example = [
+["MyLinkedList","addAtHead","addAtTail","addAtIndex","get","deleteAtIndex","get"]
+,[[],[1],[3],[1,2],[1],[0],[0]]]
+
+obj = MyLinkedList()
+for idx, op in enumerate(example[0]):
+    val = example[1][idx]
+    if idx == 0 :
+        continue
+    if 'Head' in op:
+        obj.addAtHead(val[0])
+        print('null')
+    elif 'Tail' in op:
+        obj.addAtTail(val[0])
+        print('null')
+    elif 'addAtIndex' in op:
+        obj.addAtIndex(val[0],val[1])
+        print('null')
+    elif 'deleteAtIndex' in op:
+        obj.deleteAtIndex(val[0])
+        print('null')
+    elif 'get' in op:
+        tmp = obj.get(val[0])
+        print(tmp)
+    
